df87ef7bacd7fd078f2b8cd9dbf5b3e3b1a11edd
Fernando Díaz <fdsolinf@gmail.com>
2024-06-04 17:27:45 -0500

    Curso Django clase 42

--- empleado/applications/empleados/urls.py
+++ empleado/applications/empleados/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listar-todo-empleados/', views.ListaAllEmpleados.as_view())
+    path('listar-todo-empleados/', views.ListaAllEmpleados.as_view()),
+    path('listar-by-area/<shortname>/', views.ListByAreaEmpleado.as_view()),
+    path('buscar-empleado/', views.ListEmpleadosByWork.as_view()),
 ]
--- empleado/applications/empleados/views.py
+++ empleado/applications/empleados/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import  ListView
 
@@ -6,4 +8,31 @@ from .models import Empleado
 
 class ListaAllEmpleados(ListView):
     template_name = 'persona/list_all.html'
+    paginate_by = 5
+    ordering = 'first_name'
     model = Empleado
+
+class ListByAreaEmpleado(ListView):
+    template_name = 'persona/list_by_area.html'
+    
+    def get_queryset(self):
+        # se recibe variable de la url
+        area = self.kwargs['shortname'] 
+        lista = Empleado.objects.filter(
+            departamento__short_name = area
+        )
+        return lista
+
+class ListEmpleadosByWork(ListView):
+    """ Lista a través de empleados """
+    template_name = 'persona/by_kword.html'
+    context_object_name = 'empleados'
+
+    def get_queryset(self):
+        print('******************')
+        palabra_clave = self.request.GET.get("kword", '')
+        lista = Empleado.objects.filter(
+            first_name = palabra_clave
+        )
+        print('lista resultado: ', lista)
+        return lista
